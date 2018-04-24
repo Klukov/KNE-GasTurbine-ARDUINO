9d270d6271f0a2e99249642ebe8c8f3d4f66bf53
Piotr Klukowski <38554247+Klukov@users.noreply.github.com>
2018-04-24 19:46:31 +0200

    Add files via upload

---
+++ TG_sterowanie/Bluetooth.ino
@@ -0,0 +1,60 @@
+int jaki_rozkaz_BT ()
+{
+  if (Serial3.available() > 1)
+  {
+    check = Serial3.read();
+    rozkaz = Serial3.read();
+    if (check == 'x')
+    {
+      if (rozkaz == 't')
+      {
+        return rozkaz_start;
+      }
+      else if (rozkaz == '+')
+      {
+        return rozkaz_power_up;
+      }
+      else if (rozkaz == '-')
+      {
+        return rozkaz_power_down;
+      }
+      else if (rozkaz == 'p')
+      {
+        return rozkaz_stop;
+      }
+      else
+      {
+        return 0;
+      }
+      czyszczenie_bufora_BT();
+    }
+    else
+    {
+      czyszczenie_bufora_BT();
+      return 0;
+    }
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void czyszczenie_bufora_BT ()
+{
+  while (Serial3.available() > 0)
+  {char x = Serial3.read();}
+}
+
+void wyslij_wartosci_BT (char x, char y) // program na kompie czyta wdl okreslonej kolejnosci
+{
+  String dane = "\r";
+  dane += x; dane += "\r\n";
+  dane += y; dane += "\r\n";
+  float wysylany_gaz = ((((float)gas_auto_level)*100.0)/ 1024.0);
+  dane += wysylany_gaz; dane += "\r\n";
+  dane += crank_rev; dane += "\r\n";
+  dane += exhoust_temp; dane += "\r\n";
+  dane += oil_press; dane += "\r\n";
+  Serial3.print(dane);
+}
---
+++ TG_sterowanie/SuperStart.ino
@@ -0,0 +1,60 @@
+int jaki_rozkaz ()
+{
+  if (Serial.available() > 1)
+  {
+    check = Serial.read();
+    rozkaz = Serial.read();
+    if (check == 'x')
+    {
+      if (rozkaz == 't')
+      {
+        return rozkaz_start;
+      }
+      else if (rozkaz == '+')
+      {
+        return rozkaz_power_up;
+      }
+      else if (rozkaz == '-')
+      {
+        return rozkaz_power_down;
+      }
+      else if (rozkaz == 'p')
+      {
+        return rozkaz_stop;
+      }
+      else
+      {
+        return 0;
+      }
+      czyszczenie_bufora();
+    }
+    else
+    {
+      czyszczenie_bufora();
+      return 0;
+    }
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void czyszczenie_bufora ()
+{
+  while (Serial.available() > 0)
+  {char x = Serial.read();}
+}
+
+void wyslij_wartosci (char x, char y) // program na kompie czyta wdl okreslonej kolejnosci
+{
+  String dane = "";
+  dane += x; dane += "\r\n";
+  dane += y; dane += "\r\n";
+  float wysylany_gaz = ((((float)gas_auto_level)*100.00)/ 1024.00);
+  dane += wysylany_gaz; dane += "\r\n";
+  dane += crank_rev; dane += "\r\n";
+  dane += exhoust_temp; dane += "\r\n";
+  dane += oil_press; dane += "\r\n";
+  Serial.print(dane);
+}
